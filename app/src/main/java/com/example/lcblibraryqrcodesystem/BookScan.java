757ef9e20f6c9837061db7e9451f95ee653b886a
wafi136 <wafi_it@outlook.com>
2019-10-28 17:02:18 +0800

    final version

---
+++ app/src/main/java/com/example/lcblibraryqrcodesystem/BookScan.java
@@ -0,0 +1,189 @@
+package com.example.lcblibraryqrcodesystem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcode;
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetector;
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetectorOptions;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.common.FirebaseVisionImageMetadata;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+import com.otaliastudios.cameraview.CameraView;
+import com.otaliastudios.cameraview.frame.Frame;
+import com.otaliastudios.cameraview.frame.FrameProcessor;
+
+import java.util.List;
+
+public class BookScan extends AppCompatActivity {
+
+    CameraView cameraView;
+    boolean Shown = false;
+    Button PressAgain;
+
+    FirebaseVisionBarcodeDetectorOptions options;
+    FirebaseVisionBarcodeDetector detector;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_scan);
+
+        Dexter.withActivity(this)
+                .withPermissions(Manifest.permission.CAMERA,Manifest.permission.RECORD_AUDIO)
+                .withListener(new MultiplePermissionsListener() {
+                    @Override
+                    public void onPermissionsChecked(MultiplePermissionsReport report) {
+                        setupCamera();
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions, PermissionToken token) {
+
+                    }
+                }).check();
+    }
+
+    private void setupCamera() {
+        PressAgain = (Button)findViewById(R.id.btn_again);
+        PressAgain.setEnabled(Shown);
+        PressAgain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Shown = !Shown;
+                PressAgain.setEnabled(Shown);
+            }
+        });
+
+        cameraView = (CameraView)findViewById(R.id.cameraView);
+        cameraView.setLifecycleOwner(this);
+        cameraView.addFrameProcessor(new FrameProcessor() {
+            @Override
+            public void process(@NonNull Frame frame) {
+                processImage(getVisionImageFromFrame(frame));
+            }
+        });
+
+        options = new FirebaseVisionBarcodeDetectorOptions.Builder()
+                .setBarcodeFormats(FirebaseVisionBarcode.FORMAT_QR_CODE)
+                .build();
+        detector = FirebaseVision.getInstance().getVisionBarcodeDetector(options);
+    }
+
+    private void processImage(FirebaseVisionImage image) {
+        if(!Shown)
+        {
+            detector.detectInImage(image)
+                    .addOnSuccessListener(new OnSuccessListener<List<FirebaseVisionBarcode>>() {
+                        @Override
+                        public void onSuccess(List<FirebaseVisionBarcode> firebaseVisionBarcodes) {
+                            processResult(firebaseVisionBarcodes);
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(BookScan.this,"Ida mau"+e.getMessage(),Toast.LENGTH_SHORT).show();
+                        }
+                    });
+        }
+    }
+
+    private void processResult(List<FirebaseVisionBarcode> firebaseVisionBarcodes) {
+        if(firebaseVisionBarcodes.size()>0)
+        {
+            Shown = true;
+            PressAgain.setEnabled(Shown);
+            for(FirebaseVisionBarcode item: firebaseVisionBarcodes)
+            {
+                int value_type = item.getValueType();
+                switch (value_type)
+                {
+                    case FirebaseVisionBarcode.TYPE_TEXT:
+                    {   //Data to be send to next activity
+                        createDialog(item.getRawValue());
+                        Intent passData = new Intent(BookScan.this,BorrowingProcess.class);
+                        passData.putExtra("Book",item.getRawValue());
+                        startActivity(passData);
+                        finish();
+                    }
+                    break;
+                    case FirebaseVisionBarcode.TYPE_URL:
+                    {
+                        //START browser intent
+                        Intent Website = new Intent(Intent.ACTION_VIEW, Uri.parse(item.getRawValue()));
+                        startActivity(Website);
+                    }
+                    break;
+                    case FirebaseVisionBarcode.TYPE_CONTACT_INFO:
+                    {
+                        String info = new StringBuilder("Name:  ")
+                                .append(item.getContactInfo().getName().getFormattedName())
+                                .append("\n")
+                                .append("Address:  ")
+                                .append(item.getContactInfo().getAddresses().get(0).getAddressLines()[0])
+                                .append("\n")
+                                .append("Email: ")
+                                .append(item.getContactInfo().getEmails().get(0).getAddress())
+                                .toString();
+                        createDialog(info);
+                    }
+                    break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+
+    private void createDialog(String text) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(text)
+                .setPositiveButton("Accept", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        //Intent intent = new Intent(BookScan.this,BorrowingProcess.class); startActivity(intent);
+
+                    }
+                });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    private FirebaseVisionImage getVisionImageFromFrame(Frame frame) {
+        byte[] data = frame.getData();
+        FirebaseVisionImageMetadata metadata = new FirebaseVisionImageMetadata.Builder()
+                .setFormat(FirebaseVisionImageMetadata.IMAGE_FORMAT_NV21)
+                .setHeight(frame.getSize().getHeight())
+                .setWidth(frame.getSize().getWidth())
+                .build();
+        return FirebaseVisionImage.fromByteArray(data,metadata);
+
+
+
+
+
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/lcblibraryqrcodesystem/Menu.java
@@ -0,0 +1,73 @@
+package com.example.lcblibraryqrcodesystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.hitomi.cmlibrary.CircleMenu;
+import com.hitomi.cmlibrary.OnMenuSelectedListener;
+import com.hitomi.cmlibrary.OnMenuStatusChangeListener;
+
+public class Menu extends AppCompatActivity {
+    String arrayIcon[]  =
+            {"Login","Register", "About"};
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+        CircleMenu circleMenu = findViewById(R.id.circle_menu);
+
+
+
+        circleMenu.setMainMenu(Color.parseColor("#D3D3D3"),R.drawable.ic_add,R.drawable.ic_clear)
+                .addSubMenu(Color.parseColor("#ffffff"),R.drawable.bleach)
+                .addSubMenu(Color.parseColor("#4169E1"),R.drawable.facebook_icons8)
+                .addSubMenu(Color.parseColor("#32CD32"),R.drawable.whatsapp_icons8)
+
+
+                .setOnMenuSelectedListener(new OnMenuSelectedListener() {
+                    @Override
+                    public void onMenuSelected(int Visit) {
+                        switch (Visit) {
+                            case 0:
+                                Toast.makeText(Menu.this,"Click this "+ arrayIcon[Visit],Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(Menu.this,Login.class);
+                                startActivity(intent);
+                                break;
+                            case 1:
+                                Toast.makeText(Menu.this,"Click this "+ arrayIcon[Visit],Toast.LENGTH_SHORT).show();
+                                Intent intent2 = new Intent(Menu.this,Register.class);
+                                startActivity(intent2);
+                                break;
+                            case 2:
+                                Toast.makeText(Menu.this,"Click this "+ arrayIcon[Visit],Toast.LENGTH_SHORT).show();
+                                Intent intent3 = new Intent(Menu.this,BookScan.class);
+                                startActivity(intent3);
+                                break;
+
+                            default:
+                                Toast.makeText(Menu.this, "Notihing", Toast.LENGTH_SHORT).show();
+                        }}
+                }) .setOnMenuStatusChangeListener(new OnMenuStatusChangeListener() {
+            @Override
+            public void onMenuOpened() {
+                Log.e("tag","onOpened");
+            }
+
+            @Override
+            public void onMenuClosed() {
+                Log.e("tag","onClosed");
+            }
+
+
+        });
+
+    }
+}
